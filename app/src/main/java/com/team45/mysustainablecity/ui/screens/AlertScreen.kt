769c667864a37b4ef823233bb2778872ac610aa0
Lovin Sharma <130526452+Shawarm0@users.noreply.github.com>
2026-02-26 05:51:09 +0000

    Completed LoginScreen.kt and SignUpScreen.kt UI

--- app/src/main/java/com/team45/mysustainablecity/ui/screens/AlertScreen.kt
+++ app/src/main/java/com/team45/mysustainablecity/ui/screens/AlertScreen.kt
@@ -1,10 +1,7 @@
 package com.team45.mysustainablecity.ui.screens
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
