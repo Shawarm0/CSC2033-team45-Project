778a662dc1634fb7b5956909e1d392743f0ce9a4
Omeir <op6666667@gmail.com>
2026-02-26 14:24:59 +0000

    Created unfinished registerUser and loginUser functions

--- app/src/main/java/com/team45/mysustainablecity/reps/userRep.kt
+++ app/src/main/java/com/team45/mysustainablecity/reps/userRep.kt
@@ -3,11 +3,24 @@ package com.team45.mysustainablecity.reps
 import com.team45.mysustainablecity.data.classes.User
 import com.team45.mysustainablecity.data.remote.SupabaseClientProvider
 import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.exception.AuthRestException
 import io.github.jan.supabase.auth.providers.builtin.Email
 
 class UserRep {
     private val client = SupabaseClientProvider.client
 
 
+    fun registerUser(user: User): Int {
+        TODO("Register a user")
+    }
+
+    //suspend because needs to pause without blocking main thread
+    suspend fun loginUser(email: String, password: String) {
+        try {
+            client.auth.signInWith(Email)
+        } catch (e: AuthRestException) {
+
+        }
+    }
 
 }
