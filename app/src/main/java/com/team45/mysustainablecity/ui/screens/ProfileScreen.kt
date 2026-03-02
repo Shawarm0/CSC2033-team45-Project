229c4755ba2de113f2c1ce6b946f8ad23450a976
alexi <c3073816@newcastle.ac.uk>
2026-03-02 11:21:18 +0000

    Made changes to Profile Screen

--- app/src/main/java/com/team45/mysustainablecity/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/team45/mysustainablecity/ui/screens/ProfileScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
