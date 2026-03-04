ee1c974d00810206a2263fe91cafcf61cab32755
alexi <c3073816@newcastle.ac.uk>
2026-03-04 18:25:05 +0000

    Added data class and last hour alerts

--- app/src/main/java/com/team45/mysustainablecity/ui/screens/AlertScreen.kt
+++ app/src/main/java/com/team45/mysustainablecity/ui/screens/AlertScreen.kt
@@ -4,9 +4,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
+data class AlertItem(
+    val message: String,
+    val timeAgo: String
+)
 @Composable
 fun AlertScreen(
     navController: NavController
 ) {
+    val lastHourAlerts = listOf(
+        AlertItem("user 1 liked your post", "5 mins ago"),
+        AlertItem("user 2 commented on your post - \"This is sick...", "10 mins ago"),
+        AlertItem("user 3 liked your post", "15 mins ago"),
+        AlertItem("user 4 commented on your post - \"Yeah the po...", "6 mins ago"),
+
+
+    )
     Text("Alert Screen")
 }
