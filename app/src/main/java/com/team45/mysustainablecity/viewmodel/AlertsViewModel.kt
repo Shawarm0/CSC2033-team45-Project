b4c2763f6ab126d246569a7b96074752a9f9de6a
EnisBerkayBASHAN <enis.berkay.bashan@gmail.com>
2026-03-08 15:55:44 +0000

    markAlertAsRead and loading errorstate

--- app/src/main/java/com/team45/mysustainablecity/viewmodel/AlertsViewModel.kt
+++ app/src/main/java/com/team45/mysustainablecity/viewmodel/AlertsViewModel.kt
@@ -14,19 +14,61 @@ class AlertsViewModel (
 ): ViewModel(){
     private val _alerts = MutableStateFlow<List<Alert>>(emptyList())
     val alerts: StateFlow<List<Alert>> = _alerts
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+    private val _errorState = MutableStateFlow<String?>(null)
+    val errorState: StateFlow<String?> = _errorState
     /**
      * Load alerts for the user
      */
     fun loadAlerts(userId: String?) {
         viewModelScope.launch {
+
+            _isLoading.value = true
+            _errorState.value = null
+
             try {
                 userRep.getAlerts(userId).collect { alertsList ->
-                    Log.d("AuthViewModel", "successfully loaded alerts $alertsList")
+                    Log.d("AlertsViewModel", "Successfully loaded alerts: $alertsList")
                     _alerts.value = alertsList
                 }
 
             } catch (e: Exception) {
-                Log.e("AuthViewModel", "Failed to load alerts: ${e.message}")
+                Log.e("AlertsViewModel", "Failed to load alerts: ${e.message}")
+                _errorState.value = e.message
+
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
+
+
+    /**
+     * Mark alert as read
+     */
+    fun markAlertAsRead(alertId: String?) {
+        viewModelScope.launch {
+
+            _isLoading.value = true
+            _errorState.value = null
+
+            try {
+                val success = userRep.markAlertRead(alertId)
+
+                if (success) {
+                    Log.d("AlertsViewModel", "Alert marked as read")
+                }else{
+                    _errorState.value = "Failed to mark alert as read"
+                    Log.e("AlertsViewModel", "Failed to mark alert as read")
+                }
+
+            } catch (e: Exception) {
+                Log.e("AlertsViewModel", "Failed to mark alert as read: ${e.message}")
+                _errorState.value = e.message
+
+            } finally {
+                _isLoading.value = false
             }
         }
     }
